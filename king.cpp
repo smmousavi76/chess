053dc8346609eefe472ad577b0188b2de32fa3e4
hanietaheri <st.honeyeh@gmail.com>
2016-06-23 13:26:42 +0430

    king move fixed

--- king.cpp
+++ king.cpp
@@ -12,45 +12,63 @@ King::King(int owner):Piece( owner)
     }
      if(owner == 1)//black
     {
-        std::cout<<"hhhhhh";
+        //std::cout<<"hhhhhh";
         typeId=8;
         //Black_White=1;
         pos.xPos =3;
         pos.yPos = 7;
-
     }
-
 }
 std::vector <Posiotion> King::PossibleMove()
 {
     std::vector<Posiotion> possibleMoves;
     int First_x=pos.xPos;
     int First_y=pos.yPos;
+
     Posiotion a;
+
     a.xPos=pos.xPos;
     a.yPos=pos.yPos+1;
     possibleMoves.push_back(a);
-    a.yPos=pos.yPos+1;
-    possibleMoves.push_back(a);
+    a.xPos=First_x;
+    a.yPos=First_y;
     a.xPos=pos.xPos+1;
-    a.yPos=pos.yPos;
     possibleMoves.push_back(a);
+    a.xPos=First_x;
+    a.yPos=First_y;
     a.xPos=pos.xPos-1;
-    a.yPos=pos.yPos;
     possibleMoves.push_back(a);
+    a.xPos=First_x;
+    a.yPos=First_y;
     a.xPos=pos.xPos+1;
     a.yPos=pos.yPos+1;
     possibleMoves.push_back(a);
+    a.xPos=First_x;
+    a.yPos=First_y;
     a.xPos=pos.xPos-1;
     a.yPos=pos.yPos+1;
     possibleMoves.push_back(a);
+    a.xPos=First_x;
+    a.yPos=First_y;
     a.xPos=pos.xPos+1;
     a.yPos=pos.yPos-1;
     possibleMoves.push_back(a);
+    a.xPos=First_x;
+    a.yPos=First_y;
     a.xPos=pos.xPos-1;
+    a.yPos=pos.yPos-1;
+    possibleMoves.push_back(a);
+    a.xPos=First_x;
+    a.yPos=First_y;
+    a.xPos=pos.xPos-1;
+    a.yPos=pos.yPos+1;
+    possibleMoves.push_back(a);
+    a.xPos=First_x;
+    a.yPos=First_y;
     a.yPos=pos.yPos-1;
     possibleMoves.push_back(a);
 
+    return possibleMoves;
 }
 King::~King()
 {
