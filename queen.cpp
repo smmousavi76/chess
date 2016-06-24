cc5e19243bf598722485604bd18e5bb92d7a4b56
hanietaheri <st.honeyeh@gmail.com>
2016-06-24 18:12:58 +0430

    queen fixed for both

--- queen.cpp
+++ queen.cpp
@@ -16,7 +16,7 @@ Queen::Queen(int owner):Piece( owner)
     else if(owner == 1)//black
     {
         typeId=9;
-    std::cout<<"salam";
+   // std::cout<<"salam";
       //  Black_White=1;
         pos.xPos =4;
         pos.yPos = 7;
@@ -28,7 +28,8 @@ std::vector <Posiotion> Queen:: PossibleMove(int owner)
     std::vector<Posiotion> possibleMoves;
     int First_x=pos.xPos;
     int First_y=pos.yPos;
-    for(int i=pos.yPos;i<=8;i++)
+    if (owner == 0){
+        for(int i=pos.yPos;i<=8;i++)
     {
 
         Posiotion a;
@@ -75,6 +76,57 @@ std::vector <Posiotion> Queen:: PossibleMove(int owner)
     }
 
     return possibleMoves;
+    }
+    else {
+        for(int i=pos.yPos;i<=8;i++)
+    {
+
+        Posiotion a;
+        a.yPos=i;
+        a.xPos=First_x;
+        possibleMoves.push_back(a);
+    }
+        pos.xPos=First_x;
+        pos.yPos=First_y;
+    for(int i=pos.yPos;i>=0;i--)
+    {
+        Posiotion b;
+        b.xPos=First_x;
+        b.yPos=i;
+        possibleMoves.push_back(b);
+    }
+    pos.xPos=First_x;
+    pos.yPos=First_y;
+    for(int i=pos.xPos;i<=8;i++)
+    {
+        Posiotion c;
+        c.xPos=i;
+        c.yPos=First_y;
+        possibleMoves.push_back(c);
+    }
+    pos.xPos=First_x;
+    pos.yPos=First_y;
+    for(int i=pos.xPos;i>=0;i--)
+    {
+        Posiotion d;
+        d.xPos=i;
+        d.yPos=First_y;
+        possibleMoves.push_back(d);
+    }
+
+    pos.xPos=First_x;
+    pos.yPos=First_y;
+    for(int i=8;i>0;i--)
+    {
+        possibleMoves.push_back(Posiotion(First_x+i,First_y+i));
+        possibleMoves.push_back(Posiotion(First_x+i,First_y-i));
+        possibleMoves.push_back(Posiotion(First_x-i,First_y+i));
+        possibleMoves.push_back(Posiotion(First_x-i,First_y-i));
+    }
+
+    return possibleMoves;
+    }
+
 }
 
 Queen::~Queen()
