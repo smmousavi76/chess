09b676b1655a24809ebb1d6100de8c6288d2edb0
Mohammad Reza <sm.mousavi90@yahoo.com>
2016-06-29 12:18:58 +0430

    cleaning parser

--- update.cpp
+++ update.cpp
@@ -20,171 +20,18 @@ bool Update::playerChanger(Posiotion from,Posiotion to)
     ///send "data" using socket
     b=whichPiece(req.from);
     if(b->owner==0)
-        data[1]='0';
+        data[0]='0';
     if(b->owner==1)
-        data[1]='1';
-
         data[0]='1';
-        /*
-    switch(from.xPos)
-    {
-        case 1:
-        data[2]='0';
-        break;
-        case 2:
-        data[2]='1';
-        break;
-        case 3:
-        data[2]='2';
-        break;
-        case 4:
-        data[2]='3';
-        break;
-        case 5:
-        data[2]='4';
-        break;
-        case 6:
-        data[2]='5';
-        break;
-        case 7:
-        data[2]='6';
-        break;
-        default :data[2]='7';
-        break;
-    }
-    switch (from.yPos)
-    {
-        case 1:
-        data[3]='0';
-        break;
-        case 2:
-        data[3]='1';
-        break;
-        case 3:
-        data[3]='2';
-        break;
-        case 4:
-        data[3]='3';
-        break;
-        case 5:
-        data[3]='4';
-        break;
-        case 6:
-        data[3]='5';
-        break;
-        case 7:
-        data[3]='6';
-        break;
-        default: data[3]='7';
-        break;
-    }
-     switch(to.xPos)
-    {
-        case 1:
-        data[4]='0';
-        break;
-        case 2:
-        data[4]='1';
-        break;
-        case 3:
-        data[4]='2';
-        break;
-        case 4:
-        data[4]='3';
-        break;
-        case 5:
-        data[4]='4';
-        break;
-        case 6:
-        data[4]='5';
-        break;
-        case 7:
-        data[4]='6';
-        break;
-        default :data[4]='7';
-        break;
-    }
-    switch (to.yPos)
-    {
-        case 1:
-        data[5]='0';
-        break;
-        case 2:
-        data[5]='1';
-        break;
-        case 3:
-        data[5]='2';
-        break;
-        case 4:
-        data[5]='3';
-        break;
-        case 5:
-        data[5]='4';
-        break;
-        case 6:
-        data[5]='5';
-        break;
-        case 7:
-        data[5]='6';
-        break;
-        default: data[5]='7';
-        break;
-    }
+        data[1]=b->typeId+48;
+        data[2]=req.from.xPos+48;
+        data[3]=req.from.yPos+48;
+        data[4]=req.to.xPos+48;
+        data[5]=req.to.yPos+48;
 
-
-
-    switch(b->typeId){
-    case 1 : data[0]='1';
-    break;
-    case 2 : data[0]='2';
-    break;
-    case 3 : data[0]='3';
-    break;
-    case 4 : data[0]='4';
-    break;
-    case 5 : data[0]='5';
-    break;
-    case 6 : data[0]='6';
-    break;
-    case 7 : data[0]='7';
-    break;
-    case 8 : data[0]='8';
-    break;
-    case 9 : data[0]='9';
-    break;
-    case 10 : data[0]='10';
-    break;
-    case 11 : data[0]='11';
-    break;
-    case 12 : data[0]='12';
-    break;
-    case 13 : data[0]='13';
-    break;
-    case 14 : data[0]='14';
-    break;
-    case 15 : data[0]='15';
-    break;
-    case 16 : data[0]='16';
-    break;
-
-    }
-
-
-    if (socket.send(data, 30) != sf::Socket::Done)
-    {
-     cout<<" error in sending data \n";
-     flag=0;
-    }
-    if(flag==0)
-        return 0;
-    else if(flag==1)
-        return 1;
-*/
     sf::IpAddress recipient = "192.168.1.106";
     unsigned short port = 54000;
     socket.send(data, 100, recipient, port);
-
-//    playerTurn = (playerTurn+1)%2;
 }
 void Update::recieveData()
 {
@@ -193,152 +40,29 @@ void Update::recieveData()
     char data[30];
     std::size_t received;
     bool flag=1;
-    /*
-   if (socket.receive(data, 100, received) != sf::Socket::Done)
-    {
-      cout<< "error in receieved data  \n";
-      flag=0;
-
-    }
-    */
-    string st="192.168.1.103";
+    string st="192.168.200.1";
     sf::IpAddress sender=st;
-    unsigned short port;
+    unsigned short port=54000;
     socket.receive(data, 30, received, sender, port);
 
-    if(data[1]=='0')
-    a->owner=0;
-    if(data[1]=='1')
-    a->owner=1;
-    b->typeId=a->typeId;
-    switch(data[2])
-    {
-    case '0':
-    a->pos.xPos=0;
-    case '1':
-    a->pos.xPos=1;
-    case '2':
-    a->pos.xPos=2;
-    case '3':
-    a->pos.xPos=3;
-    case '4':
-    a->pos.xPos=4;
-    case '5':
-    a->pos.xPos=5;
-    case '6':
-    a->pos.xPos=6;
-    case '7':
-    a->pos.xPos=7;
-    }
-    switch(data[3])
-    {
-    case '0':
-    a->pos.yPos=0;
-    case '1':
-    a->pos.yPos=1;
-    case '2':
-    a->pos.yPos=2;
-    case '3':
-    a->pos.yPos=3;
-    case '4':
-    a->pos.yPos=4;
-    case '5':
-    a->pos.yPos=5;
-    case '6':
-    a->pos.yPos=6;
-    case '7':
-    a->pos.yPos=7;
-    }
-    switch(data[0])
-    {
-    case '0':
-    a->typeId=0;
-    case '1':
-    a->typeId=1;
-    case '2':
-    a->typeId=2;
-    case '3':
-    a->typeId=3;
-    case '4':
-    a->typeId=4;
-    case '5':
-    a->typeId=5;
-    case '6':
-    a->typeId=6;
-    case '7':
-    a->typeId=7;
-    case '8':
-    a->typeId=8;
-    case '9':
-    a->typeId=9;
-    case '10':
-    a->typeId=10;
-    case '11':
-    a->typeId=11;
-    case '12':
-    a->typeId=12;
-    case '13':
-    a->typeId=13;
-    case '14':
-    a->typeId=14;
-    case '15':
-    a->typeId=15;
-    }
-     switch(data[4])
-    {
-    case '0':
-    b->pos.xPos=0;
-    break;
-    case '1':
-    b->pos.xPos=1;
-    break;
-    case '2':
-    b->pos.xPos=2;
-    break;
-    case '3':
-    b->pos.xPos=3;
-    break;
-    case '4':
-    b->pos.xPos=4;
-    break;
-    case '5':
-    b->pos.xPos=5;
-    break;
-    case '6':
-    b->pos.xPos=6;
-    break;
-    case '7':
-    b->pos.xPos=7;
-    break;
-    }
-    switch(data[5])
-    {
-    case '0':
-    b->pos.yPos=0;
-    break;
-    case '1':
-    b->pos.yPos=1;
-    break;
-    case '2':
-    b->pos.yPos=2;
-    break;
-    case '3':
-    b->pos.yPos=3;
-    break;
-    case '4':
-    b->pos.yPos=4;
-    break;
-    case '5':
-    b->pos.yPos=5;
-    break;
-     case '6':
-    b->pos.yPos=6;
-    break;
-    case '7':
-    b->pos.yPos=7;
-    break;
-    }
+    int a_data,b_data,c_data,d_data,e_data;
     int owner=a->owner;
+    if(data[0]=='0')
+    a->owner=0;
+    if(data[0]=='1')
+    a->owner=1;
+    a_data=data[1];//1=typeid
+    b_data=data[2];//2=xpos from
+    c_data=data[3];//3=ypos from
+    d_data=data[4];//4 xpos to
+    e_data=data[5];//5= ypos to
+
+    a->typeId=a_data-48;
+    b->typeId=a->typeId;
+    a->pos.xPos=b_data-48;
+    a->pos.yPos=c_data-48;
+    b->pos.xPos=d_data-48;
+    b->pos.yPos=e_data-48;
 
     for(int i=0;i<=player[owner]->pieces.size();i++)
     {
@@ -348,11 +72,9 @@ void Update::recieveData()
             player[owner]->pieces[i]->pos.yPos==b->pos.yPos;
 
     }
-    }
-   // if(flag==1)
-    //return 1;
-    //else if(flag==0)
         return ;
+}
+
 }
 
 Update::Update()
@@ -364,7 +86,7 @@ Update::Update()
         int i;
         //cout<<" player \n";
        // cin>>i;
-    playerTurn = 0;
+    playerTurn = 1;
     phase = -1;
 
 
@@ -424,7 +146,7 @@ void Update::getEvent(MouseEvent& mouse,Data& data)
         phaseChanger();
         return;
     }
-    //recieveData();
+    ///recieveData();
     if(mouse.clicked)
     {
 
@@ -444,7 +166,8 @@ void Update::getEvent(MouseEvent& mouse,Data& data)
                         {
                             if(a->owner!=target->owner)
                             {
-                                ///pawn attack bishop
+
+
                                 if(a->typeId!=3||a->typeId!=9||a->typeId!=2 ||a->typeId!=8)
                                     data.possibleMoves.push_back(po);
                             }
@@ -496,6 +219,7 @@ void Update::getEvent(MouseEvent& mouse,Data& data)
                 }
             }
         }
+
     }
     return;
 }
